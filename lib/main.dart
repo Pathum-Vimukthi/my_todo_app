7aa7e2ff5d1f1cb778a12ee049bcc102c8d8afad
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-01-21 23:44:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'To Do App',
+      home: ToDoList(),
+    );
+  }
+}
